65ceceaeda9c2b670d7e29ddfd62c726e9ed33f3
DanilVelichko <velichko.danil@gmail.com>
2023-02-11 13:36:11 +0100

    autocomplete for Login password input

--- src/pages/LogIn/Login.jsx
+++ src/pages/LogIn/Login.jsx
@@ -45,6 +45,7 @@ const Login = () => {
       <Form.Item
         label="Password"
         name="password"
+        autoComplete="on"
         rules={[{ required: true, message: 'Please input your password!' }]}
       >
         <Input.Password />

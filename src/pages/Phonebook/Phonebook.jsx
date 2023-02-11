a56185e3e1b5182b55fd414048c38d8544a987f7
DanilVelichko <velichko.danil@gmail.com>
2023-02-11 14:08:15 +0100

    proptypes

--- src/pages/Phonebook/Phonebook.jsx
+++ src/pages/Phonebook/Phonebook.jsx
@@ -1,5 +1,5 @@
 import { Container } from 'components/App.styled';
-
+import PropTypes from 'prop-types';
 import { Suspense, lazy } from 'react';
 import Form from 'components/Form/Form';
 import Filter from 'components/Filter/Filter';
@@ -20,4 +20,16 @@ const Phonebook = ({ clickSubmit, onDataUpdate, arrContacts, onDeleteBtn }) => {
   );
 };
 
+Phonebook.propTypes = {
+  arrContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDataUpdate: PropTypes.func.isRequired,
+  clickSubmit: PropTypes.func.isRequired,
+  onDeleteBtn: PropTypes.func.isRequired,
+};
 export default Phonebook;
